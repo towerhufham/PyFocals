4c7a3d81672fe2be1da145dc19317183170f47a6
towerhufham <towerhufham@yahoo.com>
2019-04-24 22:56:49 -0400

    Initialized binding.py

---
+++ binding.py
@@ -0,0 +1,10 @@
+bindings = {"Left Wink": None,
+            "Right Wink": None,
+            "Eyebrows Up": None,
+            "Open Mouth": None,
+            "Head Left": None,
+            "Head Right": None,
+            "Head Up": None,
+            "Head Down": None
+}
+
