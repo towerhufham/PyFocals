dd4b0670421b2a1eb3fe6f261497479f7633c5bf
towerhufham <towerhufham@yahoo.com>
2019-04-26 00:22:53 -0400

    implemented autopy

--- binding.py
+++ binding.py
@@ -1,3 +1,5 @@
+import autopy
+
 bindings = {"Left Wink": None,
             "Right Wink": None,
             "Eyebrows Up": None,
@@ -8,3 +10,14 @@ bindings = {"Left Wink": None,
             "Head Down": None
 }
 
+def pressBoundKey(motion):
+    """Takes in a motion, and sends the bound key signal"""
+    autopy.key.tap(bindings[motion])
+
+def bind(motion, character):
+    """Binds a character key to a motion in the bindings table"""
+    bindings[motion] = character
+
+def unbind(motion):
+    """Unbinds a motion"""
+    bindings[motion] = None
